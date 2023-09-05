2bb8fdd851865a0147b7ae7d19591c1671df2cd5
Shiina <shiinahiiragi@outlook.com>
2023-09-05 11:13:13 +0800

    init electron

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1600,
-    height: 900,
+    width: 400,
+    height: 750,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
     }
   })
 
   // and load the index.html of the app.
   if (app.isPackaged) {
     mainWindow.loadURL(path.join(__dirname, './build/index.html'));
-  }
-  else {
-    mainWindow.webContents.openDevTools();
+  } else {
+    // mainWindow.webContents.openDevTools();
     mainWindow.loadURL('http://localhost:3000/');
   }
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -33,8 +31,9 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0)
-      createWindow()
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   })
 })
 
@@ -42,5 +41,7 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 })

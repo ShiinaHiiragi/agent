2acacc83c45db43168b9d705e190aa83e6c436f0
Shiina <shiinahiiragi@outlook.com>
2023-09-05 19:14:21 +0800

    stage for react

--- src/components/ButtonUsage.js
+++ src/components/ButtonUsage.js
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import Button from '@mui/joy/Button';
+import * as React from "react";
+import Button from "@mui/joy/Button";
 
 export default function ButtonUsage() {
   return <Button variant="solid">Hello world</Button>;
--- src/modules/InputPanel.js
+++ src/modules/InputPanel.js
@@ -1,8 +1,17 @@
 import { styled } from "@mui/joy/styles";
 import Textarea from "@mui/joy/Textarea";
+import Button from '@mui/joy/Button';
+import SendIcon from "@mui/icons-material/Send";
 
 const Division = styled('div')(({ theme }) => ({
-  padding: theme.spacing(1, 2, 2, 2)
+  padding: theme.spacing(1, 2, 2, 2),
+  display: "flex",
+  flexDirection: "column",
+}));
+
+const ButtonsField = styled('div')(({ theme }) => ({
+  display: "flex",
+  marginTop: theme.spacing(2)
 }));
 
 function InputPanel() {
@@ -12,10 +21,19 @@ function InputPanel() {
         color="neutral"
         minRows={4}
         maxRows={4}
-        placeholder="Input prompts..."
+        placeholder="Input prompts here..."
         size="md"
         variant="soft"
       />
+      <ButtonsField>
+        <Button
+          loading={false}
+          loadingPosition="end"
+          endDecorator={<SendIcon />}
+          variant="solid"
+          children="Send"
+        />
+      </ButtonsField>
     </Division>
   );
 }

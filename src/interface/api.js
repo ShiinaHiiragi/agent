a6c27beed33ed7f748d90c4e77c256c4c224c786
Shiina <shiinahiiragi@outlook.com>
2023-09-09 16:19:44 +0800

    change post behavior

--- src/interface/api.js
+++ src/interface/api.js
@@ -3,7 +3,8 @@ import shuntSpawner from "./shunt"
 
 const sendPrompts = shuntSpawner(
   (prompts) => new Promise((resolve, reject) => {
-    setTimeout(resolve, 2000, {
+    // post("/ask", prompts).then(resolve).catch(reject);
+    setTimeout(resolve, 750, {
       "message": "Sure! I can help you set up your working environment. Here are a few options:\n\n1. Would you like me to turn on dark mode to create a focused and distraction-free environment? If so, I can use the command <action>turn_on_dark_mode()</action>.\n\n2. If you prefer to have some background music while working, I can play study music for you. Just let me know if you'd like me to do that by using the command <action>play_study_music()</action>.\n\n3. If you have any upcoming meetings, I can show them to you by using the command <action>show_upcoming_meetings()</action>. This will open your Calendar and display your upcoming meetings.\n\n4. Lastly, if you'd like to organize your app layout for better productivity, I can help you with that too. Just let me know and I'll use the command <action>organize_app_layout()</action> to reorganize your desktop layout.\n\nPlease let me know which options you'd like to proceed with.",
       "actions": [
         "turn_on_dark_mode()",
@@ -14,9 +15,7 @@ const sendPrompts = shuntSpawner(
     );
   }),
   (prompts) => new Promise((resolve, reject) => {
-    post("/ask", prompts)
-      .then(resolve)
-      .catch(reject);
+    post("/ask", prompts).then(resolve).catch(reject);
   })
 );
 
--- src/interface/constant.js
+++ src/interface/constant.js
@@ -4,13 +4,17 @@ const backendPort = 9000;
 const isWebview = window.require === undefined;
 const isDevMode = window.location.port === "3000";
 
-const post = (route, request) => axios.post(
+const post = (route, request) => new Promise((resolve, reject) => {
+  axios.post(
     `http://localhost:${backendPort}${route}`,
     request
-);
+  )
+    .then((res) => resolve(res.data))
+    .catch((err) => reject(err.message))
+});
 
 export default post;
 export {
-    isWebview,
-    isDevMode
+  isWebview,
+  isDevMode
 }
